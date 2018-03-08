32d7aa1ba68b1cfcdb2b95b6cada930e5c56adf7
Braxton Harris <37127344+Bwh5316@users.noreply.github.com>
2018-03-07 21:58:46 -0500

    Add files via upload

---
+++ CashShare/src/main/java/com/mycompany/cashshare/ServerController.java
@@ -0,0 +1,20 @@
+package com.mycompany.cashshare;
+
+//import java.util.ArrayList;
+import org.springframework.stereotype.Controller;
+//import org.springframework.ui.ModelMap;
+//import org.springframework.web.servlet.ModelAndView;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+/**
+ * @author Braxton Harris
+ */
+@Controller
+public class ServerController {
+    
+    public ServerController()
+    {
+       
+    }
+}
